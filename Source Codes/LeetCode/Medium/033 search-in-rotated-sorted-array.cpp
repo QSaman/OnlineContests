9aee835ba4dccd40b4cea3934ee0d158f34e9968
Saman Saadi <QSaman@users.noreply.github.com>
2025-07-09 23:02:41 -0400

    Added solutions for 33 and 81

---
+++ Source Codes/LeetCode/Medium/033 search-in-rotated-sorted-array.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+// In rotated sorted array either the left half or the right half is sorted. We use this fact
+// to find the target in O(logn)
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {            
+            int mid = (left + right ) >> 1;
+            if (target == nums[mid])
+                return mid;
+            if (nums[mid] < nums[right]) // The right half is sorted
+            {
+                if (target > nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            else    // The left half is sorted
+            {
+                if (target < nums[mid] && target >= nums[left])
+                    right = mid - 1;
+                else
+                    left = mid +1;
+            }
+        }
+        return -1;
+    }
+};
---
+++ Source Codes/LeetCode/Medium/081 search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+// In a rotated sorted array either the left half or the right half is sorted. Because there may 
+// be duplication, there are cases that we don't know which half is sofrted. For example:
+// [1, 0, 1, 1, 1] or [1, 1, 1, 0, 1]. However in [1, 1, 0, 1, 1] the right half is sorted
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {
+            const int mid = (left + right ) >> 1;
+            if (nums[mid] == target)
+                return true;
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) // In [3, 3, 0, 3, 3], the right half is sorted
+                ++left, --right;
+            else if (nums[left] <= nums[mid]) // left half is sorted
+            {
+                if (target >= nums[left] && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else    //right half is sorted
+            {
+                if (target > nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return false;        
+    }
+};
