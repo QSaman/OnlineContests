b7013055b1e063e68d8017aee5e1a065284c0d4a
Saman Saadi <QSaman@users.noreply.github.com>
2025-07-12 01:23:43 -0400

    Added a comment

--- Source Codes/LeetCode/Medium/240 Search a 2D Matrix II/m_plus_n.cpp
+++ Source Codes/LeetCode/Medium/240 Search a 2D Matrix II/m_plus_n.cpp
@@ -1,6 +1,7 @@
 // This is O(m + n) approach. The key element here is to start either
 // from matrix[m - 1][0] or from matrix[0][n - 1] so in each
-// step either a row or col is elimianted
+// step either a row or col is elimianted. Because these cells are
+// both min and max in their column or row
 
 #include <vector>
 
